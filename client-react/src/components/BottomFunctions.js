21936f76be0af9c6277bce2e6d95bdf455ef8629
suminlee_benny <39254854+suminpixel@users.noreply.github.com>
2021-11-20 20:15:35 +0900

    cpu case2

--- client-react/src/components/BottomFunctions.js
+++ client-react/src/components/BottomFunctions.js
@@ -30,8 +30,8 @@ const BottomFunctions = ({
 
   const onBenchTest = async () => {
     if(!cv) return alert('need cv')
-    await onPreTest()
-    // await onServerTest()
+    await onServerTest()
+    // await onPreTest()
     // await onServerTest()
     //     .then(()=> onPreTest())
 
